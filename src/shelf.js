49a5c0df3417cdadb07df203271db839c95bd874
Mike Gudenau <mikegudenau@gmail.com>
2023-03-24 14:33:41 -0700

    Add solutions to the shelf-test

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,43 @@
+function shelfBook(bookTitle, sciFiShelf) {
+  if (sciFiShelf.length < 3) {
+    return sciFiShelf.unshift(bookTitle);
+  }
+}
+
+// The unshift() method adds one or more elements to the
+// beginning of an array and returns the new length of the array.
+
+function unshelfBook(bookTitle, sciFiShelf) {
+  for (var i = 0; i < sciFiShelf.length; i++) {
+    if (sciFiShelf[i].title === bookTitle) {
+      sciFiShelf.splice(i, 1);
+    }
+  }
+}
+
+function listTitles(titles) {
+  var list = [];
+  for (var i = 0; i < titles.length; i++) {
+    list.push(titles[i].title);
+  }
+  return list.join(", ");
+}
+
+function searchShelf(bookShelf, title) {
+  var list = [];
+  for (var i = 0; i < bookShelf.length; i++) {
+    list.push(bookShelf[i].title);
+    if (bookShelf[i].title.includes(title)) {
+      return true;
+    }
+  }
+  return false;
+}
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf,
 };
